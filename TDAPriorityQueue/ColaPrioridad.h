e9de48b957386770cefeff2271f14450e8f24d98
HectorMF25 <68268760+HectorMF25@users.noreply.github.com>
2021-10-24 16:54:51 -0600

    tag comments cola prioridad

--- TDAPriorityQueue/ColaPrioridad.h
+++ TDAPriorityQueue/ColaPrioridad.h
@@ -1,6 +1,8 @@
 #ifndef TDAPRIORITYQUEUE_COLAPRIORIDAD_H
 #define TDAPRIORITYQUEUE_COLAPRIORIDAD_H
 #include "ListaDoble.h"
+
+//Clase cola prioridad, implementa los metodos correspondientes para una cola prioridad
 template<class T>
 class ColaPrioridad
 {
@@ -34,6 +36,7 @@ inline ColaPrioridad<T>::ColaPrioridad( Heap li, bool isMax )
 	}
 }
 
+//constructor de clase
 template<class T>
 inline ColaPrioridad<T>::ColaPrioridad(ColaPrioridad<T>*& colaCopiable)
 {
@@ -41,31 +44,37 @@ inline ColaPrioridad<T>::ColaPrioridad(ColaPrioridad<T>*& colaCopiable)
 }
 
 
-
+//destructor de clase
 template<class T>
 inline ColaPrioridad<T>::~ColaPrioridad()
 {
 	delete heap;
 }
 
+//retorna el heap
 template<class T>
 inline ListaDoble<T>* ColaPrioridad<T>::getHeap()
 {
 	return heap;
 }
 
+
+//verifica si esta vacia la cola
 template<class T>
 inline bool ColaPrioridad<T>::empty()
 {
 	return heap->getCuentaNodos() == 0;
 }
 
+
+agrega elementos a la cola
 template<class T>
 inline void ColaPrioridad<T>::add(T* value)
 {
 	heap->insertar(value);
 }
 
+//elimina y reotrna el primer elemento de la cola en un tiempo de (O(1))
 template<class T>
 inline T* ColaPrioridad<T>::pop()
 {
@@ -78,6 +87,8 @@ inline T* ColaPrioridad<T>::pop()
 	return nullptr;
 }
 
+
+//obtiene el primer elemento de la lista O(1)
 template<class T>
 inline T* ColaPrioridad<T>::front() const
 {
@@ -91,6 +102,8 @@ inline T* ColaPrioridad<T>::front() const
 	return nullptr;
 }
 
+
+//me cuenta la cantidad de nodos que contiene el arbol
 template<class T>
 inline int ColaPrioridad<T>::size() const
 {
